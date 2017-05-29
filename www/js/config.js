89414587cf68d057cb0b611596ed5705d0f1936e
GandhiVt <gandhi.vera@gmail.com>
2017-05-29 16:29:30 -0500

    1
    1

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={			
+	'ApiUrl' : "http://patraer.com/merchantapp/api",	
+	'DialogDefaultTitle' : "Control de pedisdos",
+	'pushNotificationSenderid' : "409670212325",
+	'APIHasKey' : "N102SyBalxsA6MhAHp8O3241vd6hR75GNI8AN-Y"
+};
