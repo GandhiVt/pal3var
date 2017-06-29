fda20c63c00b7e819e1188ea713e99bf3590c5e0
GandhiVt <gandhi.vera@gmail.com>
2017-06-28 21:13:32 -0500

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
-
+os
 var krms_config ={			
 	'ApiUrl' : "http://patraer.com/merchantapp/api",	
-	'DialogDefaultTitle' : "Control de pedisdos",
+	'DialogDefaultTitle' : "Control de pedidos",
 	'pushNotificationSenderid' : "409670212325",
 	'APIHasKey' : "N102SyBalxsA6MhAHp8O3241vd6hR75GNI8AN-Y"
 };
