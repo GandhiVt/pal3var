2ad03a20636695acca923590d7d29057f6c9f7bb
GandhiVt <gandhi.vera@gmail.com>
2017-06-28 21:33:37 -0500

    cambios
    al mejor estio

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 
 var krms_config ={			
-	'ApiUrl' : "http://patraer.com/merchantapp/api",	
-	'DialogDefaultTitle' : "Control de pedisdos",
+	'ApiUrl' : "http://www.patraer.com/merchantapp/api",	
+	'DialogDefaultTitle' : "Pa Levar Condutor",
 	'pushNotificationSenderid' : "409670212325",
-	'APIHasKey' : "N102SyBalxsA6MhAHp8O3241vd6hR75GNI8AN-Y"
+	'APIHasKey' : "AIzaSyBalxsA6MhAHp8O3241vd6hR75GNI8AN-Y"
 };
